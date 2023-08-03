6e6c9b041735f52cfb00ff429ef284623efacfd9
guglielmopiasenti <piasentiguglielmo@gmail.com>
2023-08-03 16:01:55 +0200

    importing scss

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,9 +2,11 @@
 <html lang="en">
   <head>
     <meta charset="UTF-8" />
-    <link rel="icon" type="image/svg+xml" href="dc-logo.png" />
+    <link rel="icon" type="image/svg+xml" href="{{asset('img/dc-logo.png')}}" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>DC Universe</title>
+    <title>{{env('APP_NAME')}} | Home</title>
+    {{-- importing assets --}}
+    @vite('resources/scss/app.scss')
   </head>
   <body>
     
