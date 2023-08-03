d069d59576d1506a2623b122a8314664baa48899
guglielmopiasenti <piasentiguglielmo@gmail.com>
2023-08-03 17:36:55 +0200

    layout extends

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,14 +1,1 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <link rel="icon" type="image/svg+xml" href="{{asset('img/dc-logo.png')}}" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>{{env('APP_NAME')}} | Home</title>
-    {{-- importing assets --}}
-    @vite('resources/js/app.js')
-  </head>
-  <body>
-    
-  </body>
-</html>
+@extends('layouts.main')
