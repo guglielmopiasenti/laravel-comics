1f513c02c66de23402cce3ce44272aa5b967e59e
guglielmopiasenti <piasentiguglielmo@gmail.com>
2023-08-03 17:15:02 +0200

    main layout

---
+++ resources/layouts/main.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <link rel="icon" type="image/svg+xml" href="{{asset('img/dc-logo.png')}}" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>{{env('APP_NAME')}} | @yield('title')</title>
+    {{-- importing assets --}}
+    @vite('resources/js/app.js')
+  </head>
+  <body>
+    {{-- Header --}}
+    @include('includes.header')
+    {{-- Main --}}
+    <main>
+      @yield('main-content')
+    </main>
+    {{-- Footer --}}
+    @include('includes.footer')
+  </body>
+</html>
