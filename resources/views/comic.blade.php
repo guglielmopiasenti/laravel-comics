c2fa0533512edd5dd8d829eec344e86b9d5e3fde
guglielmopiasenti <piasentiguglielmo@gmail.com>
2023-08-21 18:00:00 +0200

    bare bones comic

--- resources/views/comic.blade.php
+++ resources/views/comic.blade.php
@@ -3,6 +3,22 @@
 
 @section('main-content')
 
-
+<div class="container">
+  <div>
+    <img class="img-comic-page" src="{{ $comic['thumb']}}" alt="$comic['series']">
+  </div>
+  <div>
+    <div>
+      <h2>{{$comic['title']}}</h2>
+      <div>
+        <div>US Price: {{$comic['price']}} <span>AVAILABLE</span></div>
+        <div>Check Availability</div>
+      </div>
+      <p>{{$comic['description']}}</p>
+    </div>
+    <div></div>
+  </div>
+  <div></div>
+</div>
 
 @endsection
