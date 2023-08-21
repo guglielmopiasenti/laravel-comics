136ade3108a18a30ad64555cebb9b68a3727e29e
guglielmopiasenti <piasentiguglielmo@gmail.com>
2023-08-21 21:58:28 +0200

    fixing font

--- resources/views/comic.blade.php
+++ resources/views/comic.blade.php
@@ -3,50 +3,64 @@
 
 @section('main-content')
 
-<div class="container">
-  <div>
-    <img class="img-comic-page" src="{{ $comic['thumb']}}" alt="$comic['series']">
+<div class="comic-container">
+  <div class="comic-image-wrapper">
+    <img class="comic-image" src="{{ $comic['thumb'] }}" alt="$comic['series']">
   </div>
-  <div>
-    <div>
-      <h2>{{$comic['title']}}</h2>
-      <div>
-        <div>US Price: {{$comic['price']}} <span>AVAILABLE</span></div>
-        <div>Check Availability</div>
-      </div>
-      <p>{{$comic['description']}}</p>
-    </div>
-    <div>
-      <div>
-        <h1>Talent</h1>
+  <div class="comic-details">
+    <div class="comic-title-section">
+      <h3 class="comic-title">{{$comic['title']}}</h3>
+      <div class="comic-price-availability">
+        <div class="comic-price">US Price: {{$comic['price']}}</div>
+        <div class="comic-available">AVAILABLE</div>
         <div>
-          <div>Art by:</div>
-          <div>
-            {{ implode(', ', $comic['artists']) }}
+          <div class="comic-availability">
+            Check Availability
           </div>
         </div>
-        <div>
-          <div>Written by:</div>
-          <div>{{ implode(', ', $comic['writers']) }}</div>
-        </div>
       </div>
-      <div>
-        <h1>Specs</h1>
-        <div>
-          <div>Series:</div>
-          <div>{{$comic['series']}}</div>
-        </div>
-        <div>
-          <div>US Price:</div>
-          <div>{{$comic['price']}}</div>
-        </div>
-        <div>
-          <div>On Sale Date:</div>
-          <div>{{$comic['sale_date']}}</div>
-        </div>
-      </div>
+      <p class="comic-description">{{$comic['description']}}</p>
+    </div>
+    <div class="comic-advertisement-card">
+      <div>ADVERTISEMENT</div>
+      <img src="public/img/adv.jpg" alt="">
     </div>
   </div>
 </div>
+  <div class="comic-talent-section">
+    <div class="comic-container">
+      <div class="comic-talent-specs">
 
+        <div class="comic-talent">
+          <h1>Talent</h1>
+          <div class="comic-artists">
+            <div>Art by:</div>
+            <div>
+              {{ implode(', ', $comic['artists']) }}
+            </div>
+          </div>
+          <div class="comic-writers">
+            <div>Written by:</div>
+            <div>{{ implode(', ', $comic['writers']) }}</div>
+          </div>
+        </div>
+        <div class="comic-specs">
+          <h1>Specs</h1>
+          <div class="comic-series">
+            <span>Series:</span>
+            <span>{{$comic['series']}}</span>
+          </div>
+          <div class="comic-us-price">
+            <span>US Price:</span>
+            <span>{{$comic['price']}}</span>
+          </div>
+          <div class="comic-sale-date">
+            <span>On Sale Date:</span>
+            <span>{{$comic['sale_date']}}</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  
 @endsection
