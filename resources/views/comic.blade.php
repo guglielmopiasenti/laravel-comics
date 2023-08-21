13ce7faa10ebdbc15315deef96f7d823998da5cf
guglielmopiasenti <piasentiguglielmo@gmail.com>
2023-08-21 16:49:25 +0200

    comic card link

---
+++ resources/views/comic.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.main')
+@section('title', 'Comic')
+
+@section('main-content')
+
+
+
+@endsection
--- resources/views/includes/comiccard.blade.php
+++ resources/views/includes/comiccard.blade.php
@@ -1,8 +1,10 @@
 <ul class="ul-comic">
   @foreach(config('comics') as $comic)
+    <a href="{{url("/comic/$loop->index")}}">
       <li class="li-comic">
-          <img class="img-comic" src="{{ $comic['thumb'] }}" alt="{{ $comic['series'] }}">
-          {{ $comic['series'] }}
+        <img class="img-comic" src="{{ $comic['thumb'] }}" alt="{{ $comic['series'] }}">
+        {{ $comic['series'] }}
       </li>
+    </a>
   @endforeach
 </ul>
