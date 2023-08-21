857930d665593cab9f6fa53f818b5d0aa4e82ab4
guglielmopiasenti <piasentiguglielmo@gmail.com>
2023-08-21 15:18:28 +0200

    links not uploaded before, correcting

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -4,9 +4,9 @@
         <img class="img-header" src="{{ asset('img/dc-logo.png') }}" alt="DC logo">
         <ul class="ul-header">
             <li class="nav-link"> <a class="nav-link" href="#">Characters</a> </li>
-            <li class="nav-link"> <a class="nav-link" href="{{route('/')}}">Comics</a> </li>
-            <li class="nav-link"> <a class="nav-link" href="{{route('/movies')}}">Movies</a> </li>
-            <li class="nav-link"> <a class="nav-link" href="{{route('/tv')}}">TV</a> </li>
+            <li class="nav-link"> <a class="nav-link" href="{{route('home')}}">Comics</a> </li>
+            <li class="nav-link"> <a class="nav-link" href="{{route('movies')}}">Movies</a> </li>
+            <li class="nav-link"> <a class="nav-link" href="{{route('tv')}}">TV</a> </li>
             <li class="nav-link"> <a class="nav-link" href="#">Games</a> </li>
             <li class="nav-link"> <a class="nav-link" href="#">Collectibles</a> </li>
             <li class="nav-link"> <a class="nav-link" href="#">Videos</a> </li>
